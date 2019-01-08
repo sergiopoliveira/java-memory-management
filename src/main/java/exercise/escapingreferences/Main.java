132c30163d1492375500ec7adb3f7ce1430de7f9
sergiopoliveira <sergiopoliveira@outlook.com>
2019-01-08 22:08:12 +0000

    Escaping References Exercise

---
+++ src/main/java/exercise/escapingreferences/Main.java
@@ -0,0 +1,20 @@
+package exercise.escapingreferences;
+
+import exercise.escapingreferences.BookCollection;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BookCollection bc = new BookCollection();
+
+        //get price of book called Tom Jones in EUR
+        System.out.println(bc.findBookByName("Tom Jones").getPrice().convert("EUR"));
+
+    }
+}
